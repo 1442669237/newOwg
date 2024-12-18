dac8d482ae19ed49f7bd2612275721dcb2263e37
1442669237@qq.com <qichenxi5i0>
2024-12-18 11:11:24 +0800

    .env配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 引入 normalize.css
+import './assets/normalize.css';
 
 const app = createApp(App)
 
